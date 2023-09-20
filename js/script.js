6355ddaccccc0c9c39dbc7a64e35dd208aac49f9
MatePascu <matepascucci2000@gmail.com>
2023-09-19 23:12:43 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
 /* Scroll section */
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
+
 window.onscroll = () =>{
+  sections.forEach(sec => {
+    let top = window.scrollY
+    let offset = sec.offsetTop - 100 // offsetTop: Retorna la distancia del elemento actual respecto al borde superior del nodo
+    let height = sec.offsetHeight // offsetHeight: Devuelve el alto de un elemento
+    let id = sec.getAttribute('id')
+
+    if(top >= offset && top < offset + height){
+      // Active Navbar Links
+      navLinks.forEach(links => {
+        links.classList.remove('active')
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+      })
+    }
+  })
   // Header BackGround
   let header = document.querySelector('header')
-
   header.classList.toggle('bg', window.scrollY > 100)
+
+  // Remove toggle icon and navbar when click navbar links (scroll)
+  menuIcon.classList.remove('bx-x')
+  navbar.classList.remove('active')
+}
+
+// Toggle Icon Navbar
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x')
+  navbar.classList.toggle('active')
 }
